67b4fe4f4e944a8a6a76841c3d5c4014ac8f05e0
Koen <dijkslag.koen@gmail.com>
2025-01-05 01:58:47 +0100

    feat(config) removed unused testing table

--- config.lua
+++ config.lua
@@ -4,8 +4,8 @@ Config.CraftingTables = {
     {
         -- Enabled progression example
         model = "gr_prop_gr_bench_01b",
-        name = "Weapon Workbewewench",
-        coords = vector4(-75.22, -821.36, 326.17, 87.51),
+        name = "Progression Weapon Workbench",
+        coords = vector4(-81.1, -818.98, 326.18, 180.00),
         categories = {"weapons", "tools", "healing"},
         useProgression = true, 
         progression = {
@@ -16,7 +16,7 @@ Config.CraftingTables = {
             {
                 name = "Pistol",
                 category = "weapons",
-                levelRequired = 10, 
+                levelRequired = 5, 
                 duration = 5,
                 xpGained = 25,  
                 materials = {
@@ -29,88 +29,22 @@ Config.CraftingTables = {
                 }
             },
             {
-                name = "Bandage",
-                category = "healing",
-                levelRequired = 1,
-                duration = 3,
-                xpGained = 10,
-                materials = {
-                    {item = "steel", amount = 5},
-                    {item = "rubber", amount = 2}
-                },
-                result = {
-                    item = "bandage",
-                    amount = 1
-                }
-            },
-            {
-                name = "Pistol Ammo",
+                name = "Knife",
                 category = "weapons",
-                levelRequired = 7,
-                duration = 3,
-                xpGained = 7,
+                levelRequired = 1, 
+                duration = 7,
+                xpGained = 56,  
                 materials = {
-                    {item = "bandage", amount = 5},
-                    {item = "steel", amount = 2},
-                    {item = "ironoxide", amount = 2},
-                    {item = "Aluminum", amount = 2}
+                    {item = "steel", amount = 250},
+                    {item = "rubber", amount = 50}
                 },
                 result = {
-                    item = "pistol_ammo",
-                    amount = 1
-                }
-            },
-            {
-                name = "Pistol Ammo",
-                category = "weapons",
-                levelRequired = 11,
-                duration = 3,
-                xpGained = 7,
-                materials = {
-                    {item = "bandage", amount = 5},
-                    {item = "steel", amount = 2},
-                    {item = "ironoxide", amount = 2},
-                    {item = "Aluminum", amount = 2}
-                },
-                result = {
-                    item = "pistol_ammo",
+                    item = "weapon_knife",
                     amount = 1
                 }
             }
         }
     },
-    -- 
-    {
-        -- Enabled progression example
-        -- EE
-        model = "gr_prop_gr_bench_01b",
-        name = "Weapon Workbewewench",
-        coords = vector4(-81.1, -818.98, 326.18, 33.24),
-        categories = {"weapons", "tools", "healing"},
-        useProgression = true, 
-        progression = {
-            baseXP = 100,
-            multiplier = 1.3 -- Multiply the baseXP by this number for each level
-        },
-        recipes = {
-            {
-                name = "Pistol",
-                category = "weapons",
-                levelRequired = 10, 
-                duration = 5,
-                xpGained = 25,  
-                materials = {
-                    {item = "steel", amount = 5},
-                    {item = "rubber", amount = 2}
-                },
-                result = {
-                    item = "weapon_pistol",
-                    amount = 1
-                }
-            }
-        }
-    },
-    -- 
     {
         -- Disabled progression example
         model = "gr_prop_gr_bench_01b",
@@ -128,6 +62,18 @@ Config.CraftingTables = {
                 },
                 result = {
                     item = "bandage",
+                    amount = 1
+                }
+            },
+            {
+                name = "Lockpick",
+                category = "tools",
+                duration = 5,
+                materials = {
+                    {item = "steel", amount = 3}
+                },
+                result = {
+                    item = "lockpick",
                     amount = 1
                 }
             }
